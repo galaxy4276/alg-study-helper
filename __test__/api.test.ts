5519e1af0d9d64a358f9a64f31464ea0600d52fb
최은기 <galaxyhi4276@gmail.com>
2022-10-04 23:36:59 +0900

    test: api 테스트 갱신

--- __test__/api.test.ts
+++ __test__/api.test.ts
@@ -1,8 +1,10 @@
 import { getAllUserCommittedList, getUserCommittedList } from '../src/apis';
 import { writeFileSync } from 'fs';
 import { resolve } from 'path';
+import CommittedListMapper from '../src/apis/Mapper';
 
 describe('Github API 테스트', () => {
+
   it('Api 요청이 성공적으로 응답받는다.', async () => {
     const res = await getUserCommittedList('galaxy4276');
     expect(res.status).toBe(200);
@@ -10,7 +12,10 @@ describe('Github API 테스트', () => {
 
   it('유저 각각의 브랜치 커밋 항목을 받아 가공후 반환한다.', async () => {
     const commitList = await getAllUserCommittedList();
-    const jsonSampleData = JSON.stringify(commitList[0], null, 2);
+    const jsonSampleData = JSON.stringify(commitList, null, 2);
     writeFileSync(resolve(__dirname, 'sample.json'), jsonSampleData);
+
+    const mapper = new CommittedListMapper(commitList);
+    console.log(mapper.getUserCommittedList('galaxy4276'));
   })
 });
