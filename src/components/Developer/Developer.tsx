9d46f3944a8b8f451d96ebc62cca362dbd1f01b1
minesp3164 <lhd000721@gmail.com>
2022-10-04 22:27:03 +0900

    feat:개발자 칸 업데이트

---
+++ src/components/Developer/Developer.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const Developer:React.FC = () => (
+	 <div className="bg-indigo-400">
+		 <div>
+		 	<h1 className="font-bold drop-shadow-md">Developers</h1>
+		 	<div className="flex items-center gap-x-2">
+			 	<img src="https://avatars.githubusercontent.com/u/50310464?v=4" className="rounded-full  w-5 h-5"/>
+		 		galaxy4276
+			 	<i className="fa-brands fa-github"/>
+			 	<i className="fa-brands fa-instagram"/>
+		 	</div>
+		 	<div className="flex item-center gap-x-2">
+				<img src="https://avatars.githubusercontent.com/u/64721119?v=4" className="rounded-full w-5 h-5"/>
+				minesp3164
+			 	<i className="fa-brands fa-github"/>
+			 	<i className="fa-brands fa-instagram"/>
+		 	</div>
+		 </div>
+	 </div>
+	)
