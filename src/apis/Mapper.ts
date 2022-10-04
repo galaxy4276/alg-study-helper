fa2bc39849e93eaaec1b679155d599b1afa51108
최은기 <galaxyhi4276@gmail.com>
2022-10-04 23:36:39 +0900

    feat: 사용자 API 데이터를 손쉽게 가공하여 제공해주는 Mapper 클래스 작성

---
+++ src/apis/Mapper.ts
@@ -0,0 +1,28 @@
+import { userListType } from '@src/apis/user-list';
+import { GithubCommitResponse } from '@src/apis/types';
+
+
+type CommittedData = {
+  [p: string]: GithubCommitResponse[];
+}
+
+class CommittedListMapper {
+  private map: Map<userListType, GithubCommitResponse[]> = new Map();
+
+  constructor(data: CommittedData[]) {
+    data.forEach(userObject => {
+      const userBranchName = Object.keys(userObject)[0] as userListType;
+      const commitData = Object.values(userObject)[0];
+      this.map.set(userBranchName, commitData);
+    });
+  }
+
+  public getMap() { return this.map; }
+
+  public getUserCommittedList(user: userListType) {
+    return this.map.get(user);
+  }
+}
+
+
+export default CommittedListMapper;
