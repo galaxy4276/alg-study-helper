c9b707c06fbd233d6c7319e7bd5215ffc8fea431
최은기 <galaxyhi4276@gmail.com>
2022-10-05 02:18:12 +0900

    feat: Layout 배경화면 지정

--- src/components/AppLayout/AppLayout.tsx
+++ src/components/AppLayout/AppLayout.tsx
@@ -3,8 +3,8 @@ import React, { PropsWithChildren } from 'react';
 
 export const AppLayout: React.FC<PropsWithChildren> = ({ children }) => (
 	<main
-		className="w-full px-6 md:px-24 pt-8 lg:px-[230px]"
+		className="w-full px-6 md:px-24 pt-8 lg:px-[230px] bg-[#FAFAFA]"
 	>
-		{ children }
+			{ children }
 	</main>
 );
