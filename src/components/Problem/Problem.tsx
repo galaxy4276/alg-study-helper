78e023326170d5aec6c1350b0b4793e0f1172010
minesp3164 <lhd000721@gmail.com>
2022-10-04 23:40:25 +0900

    feat: 잘못된 배율 수정

--- src/components/Problem/Problem.tsx
+++ src/components/Problem/Problem.tsx
@@ -5,7 +5,7 @@ export const Problem:React.FC = () => (
 		<div>
 			Two Sum
 			<i className="fa-brands fa-python mx-1"></i>
-			<i className="mx-20">날짜</i>
+			<i className="mx-10">날짜</i>
 		</div>
 	</div>
 )
