48193dfb37a66231208b4e59f26d658a39d58bf8
minesp3164 <lhd000721@gmail.com>
2022-10-04 22:26:38 +0900

    feat: 문제를 출력할 방식

---
+++ src/components/Problem/Problem.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Problem:React.FC = () => (
+	<div>
+		<div>
+			Two Sum
+			<i className="fa-brands fa-python mx-1"></i>
+			<i className="mx-20">날짜</i>
+		</div>
+	</div>
+)
